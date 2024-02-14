1fe1fc687e79fb54bc1df2c6ea7f2c4de2612600
Mr-Gutik <017rock@gmail.com>
2024-02-14 21:21:34 +0200

    mega version two

--- main.py
+++ main.py
@@ -1,1 +1,3 @@
 print('It`s mega ultra super puperrrR test file!')
+
+print('Now this application has a super duper cool mega update and ultra version 2.0!!! I feel this energy.. The power of the Jedi! I NOW KNOW HOW GITHAB WORKS!!! Uwaaaaaaaaaaaaaa!')
